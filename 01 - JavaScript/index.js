df5e06163aeca4701c715ace9da70fe1d18abd45
Erika Silverio <cms.silverio@gmail.com>
2023-10-09 20:34:38 -0300

    commit de codigos

---
+++ 01 - JavaScript/index.js
@@ -0,0 +1,54 @@
+console.log("Hola Erika");
+
+
+// alguns tipos de variables, puede ser numero, texto o booleano(true , false)
+
+var miVariable = 5
+console.log(miVariable)
+
+var miVariable1 = "Esto es un texto"
+console.log(miVariable1)
+
+var miVariable2 = true
+console.log(miVariable2)
+
+//-------------------
+
+
+var miVariable3 = 36
+console.log("mi edad es " + miVariable3 )
+console.log(miVariable3)
+
+
+var miVariable3 = 15
+console.log("mi edad ahora es " + miVariable3 )
+
+
+
+// ------------
+
+
+const miConstante = 3
+console.log("el valor de mi constante es " +  miConstante)
+
+// -----------------
+
+var op1 = 2
+var op2 = 3
+var resultado = op1 + op2
+// var resultado = 2 + 2
+console.log( "Resultado vale: " + resultado)
+
+// CONDICIONALES
+
+let miNumero = 6
+let resultadoPregunta = miNumero == 6
+
+
+if (miNumero == 6){
+    console.log("Si mi numero vale 6")    
+}else{
+    console.log("No. Mi numero no vale 6")
+}
+
+
