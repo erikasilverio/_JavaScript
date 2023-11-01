0d72cf3a42b368a595017acbde64bf9fa6dbd516
Erika Silverio <cms.silverio@gmail.com>
2023-11-01 14:37:54 -0300

    ACTUALIZACION DEL CURSO

---
+++ 02 - JavaScript/codigo.js
@@ -0,0 +1,292 @@
+// string = "cadena de texto"
+// number = 19
+// booleano = false
+
+
+// ----------- VARIABLES ----- var, let, const
+
+// var numero = 15
+// let numero = 15
+
+
+
+// -------------  a CONST nao pode mudar, é sempre a mesma.------------
+
+// const nombre = "Erika"
+
+// alert(nombre)
+
+
+
+// ---------- let e var , sim pode modificar!!!! -----------
+
+// let numero
+// numero = 29
+// alert(numero)
+
+
+// ---------------- MULTIPLES VARIABLE ------------
+
+
+// UMA FORMA DE DECLARAR ----  let numero, numero2, numero3;
+
+// OUTRA FORMA DE DECLARAR ------ let numero = 19, numero2 = 20
+
+// OUTRA MANEIRA DE DECLARAR ----- 
+// let numero1 = 19;
+// let numero2 = 20;
+
+
+
+// ------------ NULL --- VALOR VACIO.
+
+// let numero = null;
+
+
+
+// ============== NaN -- NAO SE PODE REALIZAR A OPERACAO =============
+
+
+// let numero = 5;
+// let numero2 = "erika"
+
+// alert(numero * numero2)
+
+
+// ===================  PROMPT =================
+
+
+// let nombre = prompt("decime tu nombre");
+
+// alert("Hola " + nombre)
+
+
+// ============= OPERADORES DE ASIGNACION ==============
+
+
+// let numero = 10;
+
+// numero %= 5;
+
+// document.write(numero)
+
+
+// ========= OPERADORES ARITMETICOS  =========
+
+// =========== SUMA ( + )
+
+// numero1 = 10;
+
+// numero2 = 5;
+
+// resultado = numero1 + numero2
+
+// alert(resultado)
+
+// =====
+
+// ========= DECREMENTO ( -- )
+
+// numero1 = 10;
+// numero2 = 5;
+
+// numero1 -- ;
+
+// resultado =  numero1;
+// alert(resultado)
+
+
+// ======== DIVISION ( / )
+
+
+// numero1 = 10;
+// numero2 = 5;
+
+
+// resultado =  numero1/ numero2;
+// alert(resultado)
+
+
+// ======== EXPONENCIALIZACAO ( ** )
+
+
+// numero1 = 10;
+// numero2 = 5;
+
+
+// resultado =  numero1 ** numero2 ;
+// alert(resultado)
+
+
+
+
+// ======== INCREMENTO ( ++ )
+
+
+// numero1 = 10;
+// numero2 = 5;
+
+// numero1 ++ ;
+
+// resultado =  numero1;
+// alert(resultado)
+
+
+
+
+// ======== REMAINDER , O RESTO  ( & )
+
+
+// numero1 = 10;
+// numero2 = 5;
+
+
+
+// resultado =  numero1&numero2;
+// alert(resultado)
+
+
+
+// ======== SUBTRACAO  ( - )
+
+
+// numero1 = 10;
+// numero2 = 5;
+
+
+
+// resultado =  numero1-numero2;
+// alert(resultado)
+
+
+// ======== NEGACION UNARIA  ( - )
+
+
+// numero1 = 10;
+// numero2 = 5;
+
+// resultado =  -numero1;
+// alert(resultado)
+
+
+
+//=============================================
+
+// ==============  CONCATENACION =============  //
+
+
+// ejemplo :
+
+
+// saludo = "Hola Erika!!! ";
+// pregunta = "Como estas? ";
+
+// frase = saludo + pregunta
+
+// document.write(frase)
+
+
+
+//======= outro exemplo , quando coloca " " se entende que tudo é texto.
+
+
+// numero1 = 53;
+// numero2 = 8;
+
+// frases = " " + numero1 + numero2
+// document.write(frases)
+
+
+// PARA USAR CONCAT, HAY QUE TENER UN STRING
+
+// numero1 = "53";
+// numero2 = 8;
+
+// frases =  numero1.concat(numero2)
+// document.write(frases)
+
+
+
+// ================== OUTRO PARA CONCATENAR ============
+
+
+//nombre = "Erika Silver";
+//
+//frase = "Soy " + nombre + " y estoy caminando"
+//
+//document.write(frase)
+
+
+// ========= la mas recomentable !!!!!!!!!!!!!!
+
+
+// nombre = "Erika Silver";
+
+// frase = `Soy  ${nombre}  y 
+// 
+// estoy caminando`;
+
+
+
+// ====!!!!!!!!!!!!!  COMCATENAR COM BACKTICKS !!!! ===
+
+// frase = `<div>
+// 
+// <h1> Com esse simbolo de inicio ,
+// consigo escrever codigo "HTML"</h1>
+// 
+// </div> `;
+// 
+// document.write(frase)
+
+
+// ===== OUTRA
+
+// nombre = "Erika Silverio";
+// 
+// frase = " mi nombre es 'erika' silver";
+// 
+// document.write(frase)
+
+
+// --- OPERADORES (INTERMEDIO ) ----
+
+
+// OPERADOR DE COMPARACION (TRUE OU FALSE) ( == )
+
+
+
+// let numero = 23;
+// let numero2 = 13;
+// 
+// document.write(numero == numero2)
+
+
+
+
+// OPERADOR DE INEGUALDAD ( É DISTINTO ?) ( TRUE OU FALSE ) ( != )
+
+// let numero = 23;
+// let numero2 = 13;
+// let texto = " Texto 1 ";
+// let texto2 = " Texto 2 ";
+// 
+// document.write(texto != texto2)
+
+
+
+// OPERADOR DE IDENTIDAD ( É DISTINTO ?) ( === )
+
+let numero = 23;
+let numero2 = 13;
+let texto = " Texto 1 ";
+let texto2 = " Texto 2 ";
+
+document.write(texto != texto2)
+
+
+
+
+
+
+
