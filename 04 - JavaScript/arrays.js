82303b0a80f9f6595d9310705152123a253b2522
Erika Silverio <cms.silverio@gmail.com>
2023-11-06 20:39:12 -0300

    ARRAYS

--- 04 - JavaScript/arrays.js
+++ 04 - JavaScript/arrays.js
@@ -19,7 +19,167 @@
 
 
 
+
+
+
+
+
+
 // ========== ARRAYS ASOCIATIVOS ============
+
+
+// let pc1 = {
+//     nombre: "ErikaPC",
+//     procesador: "Intel Core I7",
+//     ram: "16GB",
+//     espacio: "1TB"
+// };
+
+
+//============
+//  let pc2 = ["ErikaPC", "Intel Core I7", "16GB", "1TB"];
+// 
+//  document.write(pc1["ram"]); 
+//=================== 
+
+
+
+// let nombre = pc1["nombre"];
+// let procesador = pc1["procesador"];
+// let ram = pc1["ram"];
+// let espacio = pc1["espacio"];
+// 
+// frase =`el nombre de mi PC es: <b>${nombre}</b> <br>
+//         el procesador es: <b>${procesador}</b> <br>
+//         la memoria ram es: <b>${ram}</b> <br>
+//         el espacio en disco es: <b>${espacio}</b>`;
+// 
+// 
+// document.write(frase); 
+
+
+
+
+
+
+
+
+
+
+// ========== BUCLES E INTERACIÓN ============
+
+
+
+
+
+
+
+// ============================ IF ==================
+
+
+
+// let numero = 0
+
+// if (numero < 10) {
+
+
+//     numero ++; // QUANDO COLOCAMOS ++ AUMENTAMOS 1 !
+
+//     document.write(numero)
+
+// }
+
+
+
+
+
+
+
+
+
+
+// ====================== WHILE ================
+
+
+//let numero = 0
+
+// while (numero < 6) {
+
+//    numero++;
+//    document.write(numero + "<br>")
+
+// }
+
+
+
+
+
+
+
+
+ // ====================== DO WHILE ===================
+
+// let numero = 0
+
+// do {
+    
+//     numero++;
+//     document.write(numero + "<br>")
+// }
+
+// while (numero > 6) 
+
+
+
+
+
+
+
+
+ // ====================== BREACK ===================
+
+// let numero = 0
+ 
+// while(numero < 1000){
+//     numero ++;
+//     document.write(numero);
+ 
+//     if(numero == 10){
+//         break;
+     
+//     }
+// }
+ 
+// document.write(" Terminou el Bucle")
+
+
+
+
+
+
+
+
+
+
+
+// ====================== FOR  ===================
+
+// DECLARAMOS, INICIALIZAMOS, ITERAMOS 
+
+// EM ( FOR ) DEFINIMOS A VARIABLE, LE DAMOS LA CONDICION Y AUMENTAMOS.
+// AQUI VEMOS QUE DENTRO DE FOR A VARIABLE ES UN VALOR, AFURA OTRO.
+
+let i = 20
+
+for (let i = 0; i < 6 ; i ++) {
+    document.write( i + "<br>");
+
+}
+
+document.write(i);
+
+// 2.47 MIN
+
 
 
 
