83d7e350093568c640e98d49b735cd55e4aa203e
Erika Silverio <cms.silverio@gmail.com>
2023-11-09 18:44:10 -0300

    Ejercicio js

---
+++ 05 - JavaScript/ejercicio2.js
@@ -0,0 +1,25 @@
+let free = false;
+
+const validarCliente = (time)=>{
+    
+    let edad = prompt("Cuel es tu edad?");
+    if(edad > 18){
+        if (time >= 2 && time < 7 && free == false){
+            alert("Podes pasar gratis porque sos la primer persona despues de las 2AM");
+            free = true;
+        } else {
+            alert(`son las ${time}:00Hs y podes pasar, pero tenes que pagar la entrada`);
+        }
+    } else { 
+        alert("No podes pasar");
+    }
+}
+
+validarCliente(23);
+validarCliente(24);
+validarCliente(0.2);
+validarCliente(0.6);
+validarCliente(1);
+validarCliente(2);
+validarCliente(2.4);
+validarCliente(3);
