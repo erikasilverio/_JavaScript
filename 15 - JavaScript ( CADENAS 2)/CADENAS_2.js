18d7ec4a0b56edb08dc06bf4eb3d4f010cbe6108
Erika Silverio <cms.silverio@gmail.com>
2023-11-19 12:24:28 -0300

    javascript

---
+++ 15 - JavaScript ( CADENAS 2)/CADENAS_2.js
@@ -0,0 +1,47 @@
+
+
+
+
+// ============ SPLIT =======
+
+
+// let cadena = "Hola como estas? ";
+// 
+// 
+// resultado = cadena.split(" ");
+// 
+// document.write(resultado[2]);
+
+
+
+
+
+
+// ====================    SPLIT =======
+
+// MOSTRA DA ONDE ARRANCA E DONDE TERMINA, EL QUE TERMINA NO SE MOSTRA,
+// SE MOSTRA UNO ANTES.
+
+
+let cadena = "ABCDEFG";
+
+
+resultado = cadena.substring(0,3);
+
+document.write(resultado);
+
+
+// ============ SPLIT =======
+
+// MOSTRA DA ONDE ARRANCA E DONDE TERMINA, EL QUE TERMINA NO SE MOSTRA,
+// SE MOSTRA UNO ANTES.
+
+
+let cadena = "ABCDEFG";
+
+
+resultado = cadena.substring(0,3);
+
+document.write(resultado);
+
+
